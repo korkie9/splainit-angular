9ab3a8325c28cb56f3db8686f0e64c953184829d
Justin Korkie <justinjkorkie@gmail.com>
2021-11-25 00:16:44 +0200

    Game timer

--- src/app/_pages/game/game.component.ts
+++ src/app/_pages/game/game.component.ts
@@ -7,11 +7,55 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
-  started!: boolean
+  started!: boolean;
+  playerWhosTurnItIs!: string;
+  playerTurn!: boolean;
+  startGameTime!: boolean;
+  time: number = 30;
+  intervalId!: any;
+  timeoutId!: any;
+  nextButton = false;
+  points = 0;
+  ended!: boolean
   constructor() {}
 
   ngOnInit(): void {
-
+    this.started = true;
+    this.playerTurn = true;
+    this.playerWhosTurnItIs = 'Greg';
+    this.startGameTime = false;
+    this.ended = false
   }
 
+  startTime(): void {
+    const interval = setInterval(() => {
+      this.time = this.time - 1;
+      console.log(this.time);
+      if (this.time < 1) {
+        this.endRound()
+        this.nextButton = true;
+      }
+    }, 1000);
+    this.intervalId = interval;
+    this.timeoutId = setTimeout(() => {
+      clearInterval(this.intervalId);
+    }, (this.time + 1) * 1000);
+    this.startGameTime = true;
+    console.log('started');
+  }
+  stopTime(): void {
+    if(this.intervalId) clearInterval(this.intervalId);
+    if(this.timeoutId) clearTimeout(this.timeoutId);
+  }
+  next(): void {
+    this.playerTurn = false
+    this.startGameTime = false
+  }
+  addPoint(): void{
+    this.points += 1
+  }
+  endRound(): void{
+    this.stopTime()
+    this.ended = true
+  }
 }
